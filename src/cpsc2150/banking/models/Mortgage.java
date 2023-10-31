75c48be9a96ecfe334e86a0dc11db389082ff26d
CampbellKemp24 <fredthehog@gmail.com>
2023-10-31 14:54:58 -0400

    Did some stuff

--- src/cpsc2150/banking/models/Mortgage.java
+++ src/cpsc2150/banking/models/Mortgage.java
@@ -10,11 +10,45 @@ package cpsc2150.banking.models;
 public class Mortgage extends AbsMortgage implements IMortgage{
 
     double payment;
-    double Rate: R - the interest rate per monthly period on the loan
+    double rate;
+    ICustomer customer;
+    double debtToIncomeRatio;
+    double principal;
+    int numberOfPayments;
+    double percentDown;
+
+    void Morgage(double costOfHome, double downPayment, int numYears, ICustomer c) {
+        customer = new ICustomer(c);
+
+        /*
+        Payment: R - The monthly payment on the mortgage
+                *          Rate: R - the interest rate per monthly period on the loan
  *          Customer: - Every loan must have an associated customer
  *          DebtToIncomeRatio: R - The ratio of the customer's total monthly debt payments (including mortgage) to their monthly income
-            *          Principal: R - The amount of the loan
+                *          Principal: R - The amount of the loan
  *          NumberOfPayments: Z - total number of payments the customer will make on the loan
  *          PercentDown : R - The percent of the house cost covered by the down payment
 
+         */
+    }
+
+    public boolean loanApproved() {
+
+    }
+
+    public double getPayment() {
+
+    }
+
+    public double getRate() {
+
+    }
+
+    public double getPrincipal() {
+
+    }
+
+    public int getYears() {
+
+    }
 }
